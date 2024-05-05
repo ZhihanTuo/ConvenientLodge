cb0a23416bba69d087bfe13f5126cb985d34a081
Zhihan Tuo <johntuozh@gmail.com>
2024-05-04 19:00:14 -0700

    Created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 {/* get username, email, password from user and creates a new user with those parameters 
 * password is hashed before being passed in to a User object */}
@@ -15,3 +16,22 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email }); // Takes time to check through database
+    // First check user, then check password
+    if (!validUser) { return next(errorHandler(404, 'User not found!')); }
+    const validPassword = bcryptjs.compareSync(password, validUser.password); // Need to compare the hashed password with bcryptjs.compareSync
+    if (!validPassword) { return next(errorHandler(401, 'Wrong credentials!')); }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); // Token consisting of user's _id and env var unique to this app
+    const { password: pass, ...rest } = validUser._doc; // Separates password from rest of user info
+    res
+      .cookie('access_token', token, { httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60 * 182)}) // Token will be not be accessible by 3rd party and expires in half a year
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error); // Handled by middleware in index.js
+  }
+}
