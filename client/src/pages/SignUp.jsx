2930332bf4b58c0de8396a675277dcb1f3e68f4d
Zhihan Tuo <johntuozh@gmail.com>
2024-05-04 18:33:54 -0700

    Finished implementing functionality of Sign Up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,76 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 
 export default function SignUp() {
+  // formData is an object manipulated using setFormData
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData(
+      {
+        ...formData, // Keep the form data
+        [e.target.id]: e.target.value, // Set the changing form's value 
+      });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent page refresh
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', // Use fetch to request api route 
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        }); 
+      const data = await res.json();
+      // If an error occurs
+      if (data.success === false) { 
+        setLoading(false);
+        setError(data.message);
+        return; 
+      }
+      setLoading(false); // Finished loading by this point
+      // If no errors, set error to null and navigate user to sign in page 
+      setError(null); 
+      navigate('/sign-in');
+    } catch(error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'> Sign Up </h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input 
+          type="text" 
+          placeholder='username' 
+          className='border p-3 rounded-lg' 
+          id='username' onChange={handleChange} 
+        />
+        <input 
+          type="email" 
+          placeholder='email' 
+          className='border p-3 rounded-lg' 
+          id='email' 
+          onChange={handleChange} 
+        />
+        <input 
+          type="password" 
+          placeholder='password' 
+          className='border p-3 rounded-lg' 
+          id='password' 
+          onChange={handleChange} 
+        />
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign Up'}
+        </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
@@ -16,6 +78,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>} {/* Error message at bottom of page */}
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': { // Forward to localhost:3000 each time api is encountered
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
