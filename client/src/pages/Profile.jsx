4532fb8bdfcbfda1f1590200fe3dcdec8a07dd9a
Zhihan Tuo <johntuozh@gmail.com>
2024-05-07 14:23:02 -0700

    Added functionality for image uploading; TODO: Omit initial successful upload message when an error occurs

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,68 @@
-import { useSelector } from "react-redux"
+import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector(state => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePercent, setFilePercent] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  /* Upload file to be used as the profile avatar and handles any errors that occur */ 
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name; // Safeguard against user adding 2 files of same name 
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100; // Gets percentage of upload 
+        setFilePercent(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => setFormData({ ...formData, avatar: downloadURL }));
+      }
+    );
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="Profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+        <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*'/>
+        <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="Profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+        <p className='text-sm self-center'>
+          {/* If fileUploadError, generate error message */}
+          {(fileUploadError) ? (<span className='text-red-700'>Image Upload Error &#40;Must be &lt;2MB&#41;</span>
+          // Else check upload progress, display progress if still uploading
+          ) : (filePercent > 0 && filePercent < 100) ? (
+            <span>{`Uploading ${filePercent}%`}</span>
+            // Else check file is uploaded successfully: generate success message if so, empty string otherwise 
+            // TODO: Omit the initial successful upload message when an error occurs
+          ) : (!fileUploadError) && (filePercent === 100) ? (
+            <span className='text-green-700'>Image Uploaded Successfully!</span>
+          ) : ( // Else empty string
+            ''
+          )}
+        </p>
+
         <input type='text' placeholder='Username' id='username' className='border p-3 rounded-lg'/>
         <input type='email' placeholder='Email' id='email' className='border p-3 rounded-lg'/>
         <input type='text' placeholder='Password' id='password' className='border p-3 rounded-lg'/>
+
         <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
       </form>
       <div className="flex justify-between mt-5">
