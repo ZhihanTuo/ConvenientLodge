7b7c32a82130df9096fe9311481484a65be5ddeb
Zhihan Tuo <johntuozh@gmail.com>
2024-05-10 11:09:31 -0700

    Added image slider to the listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
+import Listing from './pages/Listing';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
@@ -14,10 +15,12 @@ export default function App() {
   <BrowserRouter>
   <Header />
     <Routes>
-      <Route path = "/" element={<Home />} />
-      <Route path = "/sign-in" element={<SignIn />} />
-      <Route path = "/sign-up" element={<SignUp />} />
-      <Route path = "/about" element={<About />} />
+      <Route path = '/' element={<Home/>} />
+      <Route path = '/sign-in' element={<SignIn/>} />
+      <Route path = '/sign-up' element={<SignUp/>} />
+      <Route path = '/about' element={<About/>} />
+      <Route path = '/listing/:listingId' element={<Listing/>} />
+
       <Route element = {<PrivateRoute/>}>
         <Route path = "/profile" element={<Profile />} />
         <Route path = "/create-listing" element={<CreateListing />} />
