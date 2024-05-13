a697138e8bb983f83a317547dd22ccd7d9e67935
Zhihan Tuo <johntuozh@gmail.com>
2024-05-12 23:45:57 -0700

    Added About page and edited site name

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About <span className='text-slate-500'>Convenient</span><span className='text-slate-700'>Lodge</span></h1>
+      <p className='mb-4 text-slate-700 indent-4'>
+        In a competitive housing market, ConvenientLodge helps you find the most optimal choice for your next dream home. 
+      </p>
+      <p className='mb-4 text-slate-700 indent-4'>
+        ConvenientLodge seeks to facilitate an easy and intuitive experience for tenants and landlords. With a protected user authentication system, you can seamlessly purchase, sell, and rent properties with peace of mind. Sign up today to connect with listing owners or post your own!
+      </p>
+    </div>
   )
 }
