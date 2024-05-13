eec41794cb9a3bc6d637fbfa81410c510f2563d5
Zhihan Tuo <johntuozh@gmail.com>
2024-05-12 23:34:56 -0700

    Slight styling touch ups

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,8 +33,8 @@ export default function Header() {
         {/* Link to home page */}
         <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-          <span className='text-slate-400'>Convenient</span>
-          <span className='text-slate-500'>Lodge</span>
+          <span className='text-slate-500'>Convenient</span>
+          <span className='text-slate-700'>Lodge</span>
         </h1>
         </Link>
         {/* Search Bar */}
@@ -62,7 +62,7 @@ export default function Header() {
             {
               (currentUser) ? 
               (<img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="Profile"/>) 
-              : (<li className='text-slate-500 hover:underline'>Sign in</li>)
+              : (<li className='text-slate-700 hover:underline'>Sign In</li>)
             }
           </Link>
         </ul>
