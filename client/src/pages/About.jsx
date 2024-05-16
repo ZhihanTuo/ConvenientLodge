638c2aeb58b883040eca93e6347a94120408c920
Zhihan Tuo <johntuozh@gmail.com>
2024-05-16 12:51:52 -0700

    Updated about info and changed header styling

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About <span className='text-slate-500'>Convenient</span><span className='text-slate-700'>Lodge</span></h1>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Us</h1>
       <p className='mb-4 text-slate-700 indent-4'>
-        In a competitive housing market, ConvenientLodge helps you find the most optimal choice for your next dream home. 
+        Welcome to ConvenientLodge, your trusted partner in the journey to find your perfect home. At ConvenientLodge, we understand that searching for a new place to live can be both exciting and overwhelming. That's why we've designed our website to be as user-friendly and intuitive as possible, ensuring that your experience is smooth, efficient, and enjoyable from start to finish.
       </p>
       <p className='mb-4 text-slate-700 indent-4'>
-        ConvenientLodge seeks to facilitate an easy and intuitive experience for tenants and landlords. With a protected user authentication system, you can seamlessly purchase, sell, and rent properties with peace of mind. Sign up today to connect with listing owners or post your own!
+        Our platform is built with state-of-the-art technology to provide a seamless and secure user experience. We prioritize your safety and privacy by implementing robust security measures. All personal information is protected with advanced encryption protocols, and our secure user system ensures that only authorized users have access to sensitive data. You can confidently explore listings, communicate with property owners, and manage your account knowing that your information is safe with us.
+      </p>
+      <p className='mb-4 text-slate-700 indent-4'>
+        Navigating ConvenientLodge is a breeze, thanks to our clean, modern design and intuitive interface. Whether you're a first-time renter, a seasoned buyer, or a landlord looking to list a property, our website offers easy-to-use tools and features tailored to your needs. Our advanced search filters allow you to quickly find properties that match your specific criteria, while high-quality photos and detailed descriptions give you a comprehensive view of each listing.
+      </p>
+      <p className='mb-4 text-slate-700 indent-4'>
+        We are committed to making your home search as simple and enjoyable as possible. At ConvenientLodge, we don't just help you find a house; we help you find a place to call home. Join us today and discover the difference a truly user-centric property listing website can make.
       </p>
     </div>
   )
